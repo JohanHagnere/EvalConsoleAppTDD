ae576913b5ecdb11f1f70f734b24d91a6d7ddabd
Johan Hagnere <johan.hagnere@gmail.com>
2023-08-02 14:10:13 +0200

    Refacto classe convertisseur + suppresion projet console

---
+++ ConvertisseurRomain/Convertisseur.cs
@@ -0,0 +1,17 @@
+﻿namespace ConvertisseurRomain
+{
+    public static class Convertisseur
+    {
+        public static string Convertir(int chiffreArabe)
+        {
+            return chiffreArabe switch
+            {
+                <= 3 => new string('I', chiffreArabe),
+                4 => "IV",
+                5 => "V",
+                < 9 => "V" + Convertir(chiffreArabe - 5),
+                _ => "IX"
+            };
+        }
+    }
+}
--- EvalConsoleAppTest/UnitTest1.cs
+++ EvalConsoleAppTest/UnitTest1.cs
@@ -1,19 +1,7 @@
+using ConvertisseurRomain;
+
 namespace EvalConsoleAppTest
 {
-    public static class Convertisseur
-    {
-        public static string Convertir(int chiffreArabe)
-        {
-            return chiffreArabe switch
-            {
-                <= 3 => new string('I', chiffreArabe),
-                4 => "IV",
-                5 => "V",
-                < 9 => "V" + Convertir(chiffreArabe - 5),
-                _ => "IX"
-            };
-        }
-    }
 
     public class UnitTest1
     {
