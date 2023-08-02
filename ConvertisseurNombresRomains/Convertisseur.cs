0d5adca11716963ff2a70c1227f21bfb73c27cd2
Johan Hagnere <johan.hagnere@gmail.com>
2023-08-02 13:33:59 +0200

    1er commit test rouge

---
+++ ConvertisseurNombresRomains/Convertisseur.cs
@@ -0,0 +1,10 @@
+﻿namespace ConvertisseurNombresRomains
+{
+    public static class ConvertisseurNombresRomains
+    {
+        public static string Convertir()
+        {
+            return "test";
+        }
+    }
+}
--- EvalConsoleAppTest/UnitTest1.cs
+++ EvalConsoleAppTest/UnitTest1.cs
@@ -1,11 +1,23 @@
 namespace EvalConsoleAppTest
 {
+    public static class Convertisseur
+    {
+        public static string Convertir(int chiffreArabe)
+        {
+            return "test";
+        }
+    }
+
     public class UnitTest1
     {
-        [Fact]
+        [Fact(DisplayName = "ETANT DONNE le chiffre 1 " +
+                                "QUAND je le convertis en nombres romains " +
+                                "ALORS j'obtiens I")]
         public void Test1()
         {
-
+            const int chiffreArabe = 1;
+            var nombreRomain = Convertisseur.Convertir(chiffreArabe);
+            Assert.Equal("I", nombreRomain);
         }
     }
 }
