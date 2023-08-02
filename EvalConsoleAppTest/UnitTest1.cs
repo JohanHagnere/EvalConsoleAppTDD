5023517b27ce3f02935d28e09afcc6dc6efa9962
Johan Hagnere <johan.hagnere@gmail.com>
2023-08-02 13:39:50 +0200

    chiffre 3 test vert

--- EvalConsoleAppTest/UnitTest1.cs
+++ EvalConsoleAppTest/UnitTest1.cs
@@ -10,6 +10,8 @@ namespace EvalConsoleAppTest
                     return "I";
                 case 2:
                     return "II";
+                case 3:
+                    return "III";
                 default:
                     return "I";
             }
